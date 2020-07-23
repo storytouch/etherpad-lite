43ff5b99a6b7fa275f19ba4f321d87d9f866fd7b
Joas Souza <joassouzasantos@gmail.com>
2020-07-23 11:45:14 -0300

    [fix] Fix async call

--- src/static/js/pluginfw/hooks.js
+++ src/static/js/pluginfw/hooks.js
@@ -82,17 +82,17 @@ async function aCallAll(hook_name, args, cb) {
   if (!cb) cb = function () {};
   if (exports.plugins.hooks[hook_name] === undefined) return cb(null, []);
 
-  var newArray = [];
   // This should be a map.
-  await exports.plugins.hooks[hook_name].forEach(async function(hook, index){
-    let test = await hookCallWrapper(hook, hook_name, args, function (res) {
+  var promises = exports.plugins.hooks[hook_name].map(async function(hook, index){
+    return  await hookCallWrapper(hook, hook_name, args, function (res) {
       return Promise.resolve(res);
     });
-    newArray.push(test)
   });
 
+  var result = await Promise.all(promises);
+
   // after forEach
-  cb(null, _.flatten(newArray, true));
+  cb(null, _.flatten(result, true));
 }
 
 /* return a Promise if cb is not supplied */
